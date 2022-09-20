a608ccab8ee025d258a15b296afb1ee462627f43
samyAlliche <samy.alliche.22@gmail.com>
2022-09-20 15:18:40 +0200

    1.1-1.2 : import image

--- exercises/module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercises/module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import logo from './LOGO HE VINCI.png';
 
 const Header = (props) => {
   return (
     <div>
+        <img src={logo} className='' alt='Logo' />
         <h1>{props.course}</h1>
     </div>
     
