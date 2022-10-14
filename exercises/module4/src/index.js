b0b03cf55962256ef8656685654f52016e06502c
samyAlliche <samy.alliche.22@gmail.com>
2022-10-14 10:06:37 +0200

    module4 - ex c & d

--- exercises/module4/src/index.js
+++ exercises/module4/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import './index.css';
 
 import App from 'components/App/App'
+
+axios.get('http://localhost:3001/persons').then(response => {
+  const notes = response.data
+  console.log(notes)
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
